8f8bbfaee70aee0ed1eba95b44015062d21a51ef
Uko <uko@unikernel.net>
2012-03-31 17:04:06 +0300

    Moved ElementData annotation declaration into Element class

--- LogicElements/src/net/unikernel/bummel/logic_elements/Analyzer/Analyzer.java
+++ LogicElements/src/net/unikernel/bummel/logic_elements/Analyzer/Analyzer.java
@@ -2,7 +2,7 @@ package net.unikernel.bummel.logic_elements.Analyzer;
 
 import java.util.Map;
 import net.unikernel.bummel.project_model.api.BasicElement;
-import net.unikernel.bummel.project_model.api.ElementData;
+import net.unikernel.bummel.project_model.api.Element.ElementData;
 import org.openide.util.lookup.ServiceProvider;
 
 /**
--- ProjectModel/src/net/unikernel/bummel/project_model/api/Element.java
+++ ProjectModel/src/net/unikernel/bummel/project_model/api/Element.java
@@ -3,6 +3,10 @@ package net.unikernel.bummel.project_model.api;
 import java.awt.Point;
 import java.beans.PropertyChangeListener;
 import java.io.Serializable;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.List;
 import java.util.Map;
 
@@ -16,7 +20,26 @@ public interface Element extends Serializable
 	public void setCoords(Point point);
 	public List<String> getPorts();
 	public Map<String, Double> process(Map<String, Double> valuesOnPorts);
-	
 	public void addPropertyChangeListener(PropertyChangeListener listener);
 	public void removePropertyChangeListener(PropertyChangeListener listener);
+
+	/**
+	 * Annotation should be used to define the location of element's XML data
+	 * file.
+	 *
+	 * @author uko
+	 */
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.TYPE)
+	public @interface ElementData
+	{
+		/**
+		 * Path to a datafile starting from classes directory. Default:
+		 * "element_info.xml"
+		 *
+		 * @return path to element's XML data file.
+		 */
+		String dataFile() default "element_info.xml";
+	}
+
 }
