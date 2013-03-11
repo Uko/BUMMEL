ec4485ff9083f2b0547c004e167eb1ff1b50400f
Uko <uko@unikernel.net>
2013-03-11 13:07:16 +0100

    Reconfigured pom for scalatest. Added NOR tests

--- AdvancedLogicElements/src/test/scala/net/unikernel/bummel/advanced_logic_elements/NorSpec.scala
+++ AdvancedLogicElements/src/test/scala/net/unikernel/bummel/advanced_logic_elements/NorSpec.scala
@@ -1,13 +1,128 @@
-import org.scalatest.FlatSpec
+import java.util
+import net.unikernel.bummel.advanced_logic_elements.Nor.Nor
+import org.scalatest.{BeforeAndAfter, FlatSpec}
+import scala.collection.JavaConverters._
 
-class NorSpec extends FlatSpec {
+class NorSpec extends FlatSpec with BeforeAndAfter {
+
+ type Signals = util.TreeMap[String, java.lang.Double]
+
+ var nor: Nor = _
+
+  before {
+    nor = new Nor()
+  }
+
   "A NOR element" should "return 001 for 000 input" in {
-	val nor = new Nor()
-	val valuesOnPorts = new TreeMap<>()
-    valuesOnPorts.put("input1",0.)
-    valuesOnPorts.put("input2",0.)
-    valuesOnPorts.put("output",0.)
-	val result = nor.process(valuesOnPorts)
-	assert(result.get(0) === 0)
+    val valuesOnPorts = new Signals {
+      put("input1", 0)
+      put("input2", 0)
+      put("output", 0)
+    }
+
+	  val result = nor.process(valuesOnPorts).asScala
+
+    assert(result("input1") === 0)
+    assert(result("input2") === 0)
+    assert(result("output") === 1)
+  }
+
+  it should "return 001 for 001 input" in {
+    val valuesOnPorts = new Signals {
+      put("input1", 0)
+      put("input2", 0)
+      put("output", 1)
+    }
+
+    val result = nor.process(valuesOnPorts).asScala
+
+    assert(result("input1") === 0)
+    assert(result("input2") === 0)
+    assert(result("output") === 1)
+  }
+
+  it should "return 000 for 010 input" in {
+    val valuesOnPorts = new Signals {
+      put("input1", 0)
+      put("input2", 1)
+      put("output", 0)
+    }
+
+    val result = nor.process(valuesOnPorts).asScala
+
+    assert(result("input1") === 0)
+    assert(result("input2") === 0)
+    assert(result("output") === 0)
+  }
+
+  it should "return 000 for 011 input" in {
+    val valuesOnPorts = new Signals {
+      put("input1", 0)
+      put("input2", 1)
+      put("output", 1)
+    }
+
+    val result = nor.process(valuesOnPorts).asScala
+
+    assert(result("input1") === 0)
+    assert(result("input2") === 0)
+    assert(result("output") === 0)
+  }
+
+  it should "return 000 for 100 input" in {
+    val valuesOnPorts = new Signals {
+      put("input1", 1)
+      put("input2", 0)
+      put("output", 0)
+    }
+
+    val result = nor.process(valuesOnPorts).asScala
+
+    assert(result("input1") === 0)
+    assert(result("input2") === 0)
+    assert(result("output") === 0)
+  }
+
+  it should "return 000 for 101 input" in {
+    val valuesOnPorts = new Signals {
+      put("input1", 1)
+      put("input2", 0)
+      put("output", 1)
+    }
+
+    val result = nor.process(valuesOnPorts).asScala
+
+    assert(result("input1") === 0)
+    assert(result("input2") === 0)
+    assert(result("output") === 0)
+  }
+
+  it should "return 000 for 110 input" in {
+    val valuesOnPorts = new Signals {
+      put("input1", 1)
+      put("input2", 1)
+      put("output", 0)
+    }
+
+    val result = nor.process(valuesOnPorts).asScala
+
+    assert(result("input1") === 0)
+    assert(result("input2") === 0)
+    assert(result("output") === 0)
+  }
+
+  it should "return 000 for 111 input" in {
+    val valuesOnPorts = new Signals {
+      put("input1", 1)
+      put("input2", 1)
+      put("output", 1)
+    }
+
+    val result = nor.process(valuesOnPorts).asScala
+
+    assert(result("input1") === 0)
+    assert(result("input2") === 0)
+    assert(result("output") === 0)
   }
 }
+
