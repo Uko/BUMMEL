c6c6e635e79bb53f953fc69ac6c037b841e4fd40
Uko <uko@unikernel.net>
2013-03-11 13:57:04 +0100

    Added Nand spec

--- AdvancedLogicElements/src/test/scala/net/unikernel/bummel/advanced_logic_elements/NandSpec.scala
+++ AdvancedLogicElements/src/test/scala/net/unikernel/bummel/advanced_logic_elements/NandSpec.scala
@@ -1,27 +1,29 @@
+package net.unikernel.bummel.advanced_logic_elements
+
 import java.util
-import net.unikernel.bummel.advanced_logic_elements.Nor.Nor
+import net.unikernel.bummel.advanced_logic_elements.Nand.Nand
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.{BeforeAndAfter, FlatSpec}
 import scala.collection.JavaConverters._
 
-class NorSpec extends FlatSpec with ShouldMatchers with BeforeAndAfter {
+class NandSpec extends FlatSpec with ShouldMatchers with BeforeAndAfter {
 
  type Signals = util.TreeMap[String, java.lang.Double]
 
- var nor: Nor = _
+ var nand: Nand = _
 
   before {
-    nor = new Nor()
+    nand = new Nand
   }
 
-  "A NOR element" should "return 001 for 000 input" in {
+  "A NAND element" should "return 001 for 000 input" in {
     val valuesOnPorts = new Signals {
       put("input1", 0)
       put("input2", 0)
       put("output", 0)
     }
 
-    val result = nor.process(valuesOnPorts).asScala
+    val result = nand.process(valuesOnPorts).asScala
 
     result("input1") should equal (0)
     result("input2") should equal (0)
@@ -35,67 +37,67 @@ class NorSpec extends FlatSpec with ShouldMatchers with BeforeAndAfter {
       put("output", 1)
     }
 
-    val result = nor.process(valuesOnPorts).asScala
+    val result = nand.process(valuesOnPorts).asScala
 
     result("input1") should equal (0)
     result("input2") should equal (0)
     result("output") should equal (1)
   }
 
-  it should "return 000 for 010 input" in {
+  it should "return 001 for 010 input" in {
     val valuesOnPorts = new Signals {
       put("input1", 0)
       put("input2", 1)
       put("output", 0)
     }
 
-    val result = nor.process(valuesOnPorts).asScala
+    val result = nand.process(valuesOnPorts).asScala
 
     result("input1") should equal (0)
     result("input2") should equal (0)
-    result("output") should equal (0)
+    result("output") should equal (1)
   }
 
-  it should "return 000 for 011 input" in {
+  it should "return 001 for 011 input" in {
     val valuesOnPorts = new Signals {
       put("input1", 0)
       put("input2", 1)
       put("output", 1)
     }
 
-    val result = nor.process(valuesOnPorts).asScala
+    val result = nand.process(valuesOnPorts).asScala
 
     result("input1") should equal (0)
     result("input2") should equal (0)
-    result("output") should equal (0)
+    result("output") should equal (1)
   }
 
-  it should "return 000 for 100 input" in {
+  it should "return 001 for 100 input" in {
     val valuesOnPorts = new Signals {
       put("input1", 1)
       put("input2", 0)
       put("output", 0)
     }
 
-    val result = nor.process(valuesOnPorts).asScala
+    val result = nand.process(valuesOnPorts).asScala
 
     result("input1") should equal (0)
     result("input2") should equal (0)
-    result("output") should equal (0)
+    result("output") should equal (1)
   }
 
-  it should "return 000 for 101 input" in {
+  it should "return 001 for 101 input" in {
     val valuesOnPorts = new Signals {
       put("input1", 1)
       put("input2", 0)
       put("output", 1)
     }
 
-    val result = nor.process(valuesOnPorts).asScala
+    val result = nand.process(valuesOnPorts).asScala
 
     result("input1") should equal (0)
     result("input2") should equal (0)
-    result("output") should equal (0)
+    result("output") should equal (1)
   }
 
   it should "return 000 for 110 input" in {
@@ -105,7 +107,7 @@ class NorSpec extends FlatSpec with ShouldMatchers with BeforeAndAfter {
       put("output", 0)
     }
 
-    val result = nor.process(valuesOnPorts).asScala
+    val result = nand.process(valuesOnPorts).asScala
 
     result("input1") should equal (0)
     result("input2") should equal (0)
@@ -119,7 +121,7 @@ class NorSpec extends FlatSpec with ShouldMatchers with BeforeAndAfter {
       put("output", 1)
     }
 
-    val result = nor.process(valuesOnPorts).asScala
+    val result = nand.process(valuesOnPorts).asScala
 
     result("input1") should equal (0)
     result("input2") should equal (0)
