2405d073a3812eab1a88e744ba3cebecace3e80b
anygizer <mcangel@unikernel.net>
2013-05-23 19:23:24 +0300

    Added connection and processing test which produces NullPointerException.

--- ProjectModel/src/test/java/net/unikernel/bummel/project_model/LogicCircuitTest.java
+++ ProjectModel/src/test/java/net/unikernel/bummel/project_model/LogicCircuitTest.java
@@ -1,5 +1,8 @@
 package net.unikernel.bummel.project_model;
 
+import java.util.HashMap;
+import java.util.Map;
+import net.unikernel.bummel.project_model.api.BasicElement;
 import net.unikernel.bummel.project_model.api.Element;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -60,5 +63,79 @@ public class LogicCircuitTest
     instance.addElement(firstElement);
     instance.addElement(secondElement);
     assertTrue(instance.connectElements(firstElement, firstElement.getPorts().get(0), secondElement, secondElement.getPorts().get(1)));
+  }
+  @Test
+  public void testConnectElements2()
+  {
+    System.out.println("connectElements2");
+    // generator
+    Element firstElement = new BasicElement(new String[]{"output"})
+    {
+      @Override
+      public Map<String, Double> process(Map<String, Double> valuesOnPorts)
+      {
+        valuesOnPorts.put(getPort(0), Double.valueOf(1));
+        return valuesOnPorts;
+      }
+    };
+    // analyzer
+    Element secondElement = new BasicElement(new String[]{"input"})
+    {
+      @Override
+      public Map<String, Double> process(Map<String, Double> valuesOnPorts)
+      {
+        valuesOnPorts = nullFreePortsOf(valuesOnPorts);
+        if (valuesOnPorts.get(getPort(0)).compareTo(Double.valueOf(0.d)) != 0)
+        {
+          setState(1); //light it
+        } else
+        {
+          setState(0); //douse it
+        }
+        valuesOnPorts.put("input", 0.d);
+        return valuesOnPorts;
+      }
+    };
+    // not
+    Element thirdElement = new BasicElement(new String[]{"input", "output"})
+    {
+      @Override
+      public Map<String, Double> process(Map<String, Double> valuesOnPorts)
+      {
+        valuesOnPorts = nullFreePortsOf(valuesOnPorts);
+        if (valuesOnPorts.get(getPort(0)).compareTo(Double.valueOf(0.d)) == 0)
+        {
+          valuesOnPorts.put(getPort(1), Double.valueOf(1.d));
+        } else
+        {
+          valuesOnPorts.put(getPort(1), Double.valueOf(0.d));
+        }
+        valuesOnPorts.put(getPort(0), 0.d);
+        return valuesOnPorts;
+      }
+    };
+    instance.addElement(firstElement);
+    instance.addElement(secondElement);
+    instance.addElement(thirdElement);
+    assertTrue(instance.connectElements(firstElement, firstElement.getPort(0),
+            thirdElement, thirdElement.getPort(0)));
+  }
+
+  /**
+   * Test of LogicCircuit connectElements and step methods.
+   */
+  @Test
+  public void testConnectElementsAndStep1()
+  {
+    System.out.println("connectElementsAndStep1");
+    testConnectElements1();
+    for(int i = 0; i< 5; i++) instance.step();
+  }
+  @Test
+  public void testConnectElementsAndStep2()
+  {
+    System.out.println("connectElementsAndStep2");
+    testConnectElements2();
+    for(int i = 0; i< 5; i++) instance.step();
   }
 }
